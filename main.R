aef789d85ccb5fcf0f7f38d1f8793de468391d52
GranatumX <lana.garmire.group@gmail.com>
2022-09-08 07:24:09 -0400

    Updated warranty

--- main.R
+++ main.R
@@ -1,5 +1,20 @@
+# Power Analysis
+# Ching T, Huang S, Garmire LX. Power analysis and sample size estimation for RNA-Seq differential expression. RNA. 2014;20(11):1684-1696. doi:10.1261/rna.046011.114
+
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
 install.packages("BiocManager")
 BiocManager::install("edgeR")
 BiocManager::install("DESeq2")
 BiocManager::install("sSeq")
 BiocManager::install("EBSeq")
+
+# After installation run the example.r
